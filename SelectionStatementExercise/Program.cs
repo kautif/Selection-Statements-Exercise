fb0b3505cdc24d32328c014b0fa4442e11c20465
Autif Kamal <kautif@gmail.com>
2023-03-29 14:42:06 -0500

    added selection statements for guesses

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Guess my favorite number. It's between 1 and 1000");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+                userInput = int.Parse(Console.ReadLine());
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+                userInput = int.Parse(Console.ReadLine());
+            }
+            else {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
